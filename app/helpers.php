ee6922f72dfdfb2ade132b302ed7589b6bc1125a
Guillem Comes <gcomes@debelop.com>
2016-05-26 15:22:32 +0200

    Add helper function

--- app/helpers.php
+++ app/helpers.php
@@ -1,5 +1,7 @@
 <?php
 
+/* VIEW HELPERS */
+
 function label($name) {
     $key = "validation.attributes.{$name}";
     return ucfirst(trans($key));
@@ -18,6 +20,15 @@ function html_classes($a) {
 }
 
 
+
+/* DB HELPERS */
+
 function prepare_like($str) {
     return '%'.addcslashes($str, '%_').'%';
+}
+
+function get_count_from_query(\Illuminate\Database\Query\Builder $query) {
+    $totalQuery = DB::table(DB::raw("({$query->toSql()}) AS t"));
+    $totalQuery->setBindings($query->getBindings());
+    return $totalQuery->select(DB::raw('COUNT(*) AS num'))->value('num');
 }
